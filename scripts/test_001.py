07de9d6e4a45da529a44fa8ca01e058c47c637a0
luanuo <1350851934@qq.com>
2019-10-26 16:04:42 +0800

    又改成错的了

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -1,14 +1,14 @@
 # coding:utf-8
 import pytest,allure
 class Test_Abc:
-    @pytest.mark.parametrize("a",[2])
+    @pytest.mark.parametrize("a",[1,2,3])
     @allure.step(title="第一个测试用例")
     @allure.severity(allure.severity_level.BLOCKER)
     def test_001(slef,a):
         allure.attach("这是一个描述","测试描述")
         assert a == 2
 
-    @pytest.mark.parametrize("a", [4])
+    @pytest.mark.parametrize("a", [8,9,4])
     @allure.step(title="第二个测试用例")#写标题
     @allure.severity(allure.severity_level.TRIVIAL)#设置严重级别
     @allure.testcase("https://baidu.com/test_001")#测试用例的描述链接
